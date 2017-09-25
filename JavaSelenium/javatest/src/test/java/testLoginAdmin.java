9ebaf9a869bd66f824f8e325443d59e3ad8c3fd7
Mike <30903250+Yell777@users.noreply.github.com>
2017-09-25 19:38:51 +0300

    Добавлен коммент запска настроек для браузера

--- JavaSelenium/javatest/src/test/java/testLoginAdmin.java
+++ JavaSelenium/javatest/src/test/java/testLoginAdmin.java
@@ -2,9 +2,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -17,7 +21,11 @@ public class testLoginAdmin {
     private WebDriverWait wait;
     @Before
     public void start (){
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
+//        DesiredCapabilities caps = new DesiredCapabilities();
+//        caps.setCapability("unexpectedAlertBehaviour", "dismiss");
+//        WebDriver driver = new InternetExplorerDriver(caps);
+//        System.out.println(((HasCapabilities) driver).getCapabilities());
         wait = new WebDriverWait(driver,10);
     }
     @Test
