b2a2f8e32908a24eee37099d186bf0d774a9dac4
Mike <30903250+Yell777@users.noreply.github.com>
2017-10-02 15:27:52 +0300

    Revert "правка плохого коммита"
    This reverts commit 81fcb1ad830a5034f43cea778c1f09eb910ec334.

--- JavaSelenium/javatest/src/test/java/testListDuckStick.java
+++ JavaSelenium/javatest/src/test/java/testListDuckStick.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 /**
  * Created by User on 29.09.2017.
@@ -17,6 +16,7 @@ public class testListDuckStick extends TestBase {
        List<WebElement> duckList =  driver.findElements(By.cssSelector("div.box li[class^=pro]"));
 <<<<<<< HEAD
         for (int i = 0; i < duckList.size() ; i++) {
+<<<<<<< HEAD
             try {
                 duckList.get(i).findElement(By.cssSelector(" div[class^=sticker] "));
             }catch (Exception ex){
@@ -30,6 +30,10 @@ public class testListDuckStick extends TestBase {
             
             Assert.assertTrue(isElementPresent(By.cssSelector("[class^=stick]")));
 >>>>>>> parent of 77ceb6c... упс
+=======
+            duckList.get(i).findElement(By.cssSelector("[class^=stick]"));
+            Assert.assertTrue(isElementPresent(By.cssSelector("[class^=stick]")));
+>>>>>>> parent of 81fcb1a... правка плохого коммита
 
         }
 
