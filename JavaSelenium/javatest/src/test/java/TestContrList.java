958d3f2d810b922dd157845acb783c3562c64141
Mike <30903250+Yell777@users.noreply.github.com>
2017-10-02 15:10:31 +0300

    Revert "Revert "ок""
    This reverts commit 17cbe63836da9813c83f8f6424687743c9fe5aa5.

--- JavaSelenium/javatest/src/test/java/TestContrList.java
+++ JavaSelenium/javatest/src/test/java/TestContrList.java
@@ -20,9 +20,13 @@ public class TestContrList extends TestBase {
             listCou.add(coulist.get(i).findElement(By.tagName("a")).getAttribute("textContent"));
         }
         Collections.sort(listCou);
-        for (int i = 0; i < coulist.size() ; i++) {
-            String s = coulist.get(i).findElement(By.tagName("a")).getAttribute("textContent");
-            String num = coulist.get(i).findElement(By.xpath("//td[6]")).getText();
+        for (int i = 0; i < listCou.size() ; i++) {
+            System.out.println(listCou.get(i));
+        }
+
+//        for (int i = 0; i < coulist.size() ; i++) {
+//            String s = coulist.get(i).findElement(By.tagName("a")).getAttribute("textContent");
+//            String num = coulist.get(i).findElement(By.xpath("//td[6]")).getText();
 //            if (s.equals(listCou.get(i))){
 //
 //            }
@@ -38,4 +42,4 @@ public class TestContrList extends TestBase {
 
 
     }
-}
+
